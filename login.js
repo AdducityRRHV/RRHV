316c8d14813bef44da9a0e7164e22ab65815de4a
azmotorspare <adducityhv@gmail.com>
2025-01-18 02:19:06 +0500

    Update login.js
    Some change

--- login.js
+++ login.js
@@ -5,10 +5,10 @@
 document.getElementById("loginForm").addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent form from submitting
 
-    const username = document.getElementById("username").value;
+    const username = document.getElementById("").value;
     const password = document.getElementById("password").value;
 
-    if (username === VALID_USERNAME && password === VALID_PASSWORD) {
+    if (username === VALID_USERNAME && password === ) {
         alert("Login successful!");
         // Redirect to a new webpage or perform any desired action
         window.location.href = "main.html";
