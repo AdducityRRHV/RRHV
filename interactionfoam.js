62fae0530275f399c6f16696083fb3bc3dc015c6
azmotorspare <adducityhv@gmail.com>
2025-01-18 02:41:14 +0500

    Create interactionfoam.js
    add js file

---
+++ interactionfoam.js
@@ -0,0 +1,18 @@
+// Predefined credentials
+
+
+// Handle form submission
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form from submitting
+
+    const username = document.getElementById("").value;
+    const password = document.getElementById("password").value;
+
+    if (username === VALID_USERNAME && password === ) {
+        alert("Login successful!");
+        // Redirect to a new webpage or perform any desired action
+        window.location.href = "Interaction.HTML";
+    } else {
+        document.getElementById("error-message").classList.remove("hidden");
+    }
+});
